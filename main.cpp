4b18ed3fee1732a9fb2403ead09858a8cd94c9b6
atollk <tollkoetter.andreas@gmail.com>
2021-04-26 11:18:47 +0200

    Added links to SObjectizer and Boost.Fiber.

--- main.cpp
+++ main.cpp
@@ -8,34 +8,6 @@ using namespace std::chrono_literals;
 
 copper::buffered_channel<int> randoms;
 
-void work() {
-    auto r = copper::_detail::thread_local_randomizer::create();
-    std::cout << "work" << std::endl;
-    auto run = true;
-    auto next_r = std::optional<int>();
-    while (run) {
-        if (!next_r) {
-            next_r = r(10'000);
-            std::this_thread::sleep_for(1s);
-        }
-        run = copper::select(
-                  randoms <<
-                      [&next_r] {
-                          std::cout << "<" << std::flush;
-                          auto v = next_r.value();
-                          next_r.reset();
-                          return v;
-                      },
-                  randoms >>
-                      [](int x) {
-                          std::cout << ">" << std::flush;
-                          if (x % 100 == 0) {
-                              std::cout << x / 1000 << " " << std::flush;
-                          }
-                      }) == copper::channel_op_status::success;
-    }
-}
-
 int main() {
     constexpr auto N = 100;
     auto ts = std::array<std::future<void>, N>();
